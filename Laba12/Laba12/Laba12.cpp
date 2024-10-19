6b5075a61250c1c1664af457c550a8ecd997fd4d
KennyAin <narbal199111111@gmail.com>
2024-10-19 21:16:16 +0300

    laba21

---
+++ Laba12/Laba12/Laba12.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+int getPositiveIntFromUser(string title)
+{
+    char* end;
+    string input;
+    cout << title;
+    cin >> input;
+    int value = strtol(input.c_str(), &end, 10);
+    while (*end || value < 1)
+    {
+        cout << "Invalid input. Please enter an integer and more than 0: ";
+        cin >> input;
+        value = strtol(input.c_str(), &end, 10);
+    }
+    return value;
+}
+
+float getFloatFromUser(string title)
+{
+    char* end;
+    string input;
+    cout << title;
+    cin >> input;
+    float value = strtol(input.c_str(), &end, 10);
+    while (*end)
+    {
+        cout << "Invalid input. Please enter an float: ";
+        cin >> input;
+        value = strtol(input.c_str(), &end, 10);
+    }
+    return value;
+}
+
+float calculateLastValue(float d, int n, float a1)
+{
+    return a1 + (n - 1) * d;
+}
+
+float calculateProgressionSum(float d, int n, float a1)
+{
+    return ((2 * a1 + d * (n - 1)) * n) / 2;
+}
+
+int main()
+{
+    float d, a1, sum, lastValue;
+    int n;
+    d = getFloatFromUser("d: ");
+    n = getPositiveIntFromUser("n: ");
+    a1 = getFloatFromUser("a1: ");
+    lastValue = calculateLastValue(d, n, a1);
+    sum = calculateProgressionSum(d, n, a1);
+    cout << "d = " << d << endl;
+    cout << "n = " << n << endl;
+    cout << "a1 = " << a1 << endl;
+    cout << "lastValue = " << lastValue << endl;
+    cout << "sum = " << sum << endl;
+
+}
