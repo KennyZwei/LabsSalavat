c43ac2e4b0c954f609b458682e5bf403f094a21c
KennyAin <narbal199111111@gmail.com>
2024-10-19 20:44:12 +0300

    ready

--- Laba1/Laba1/Laba1.cpp
+++ Laba1/Laba1/Laba1.cpp
@@ -1,20 +1,38 @@
-﻿// Laba1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+float getFloatFromUser(string title)
+{
+    float value;
+    cout << title;
+    while (!(cin >> value)) {
+        cout << "Invalid input. Please enter an float: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    return value;
+}
+
+float calculateY(float t, float z)
+{
+    return 2 / tan(z) + t;
+}
+
+float calculateX(float t, float z, float y)
+{
+    return (3 * y * y) / (4*tan(z) - 2*t*t);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float t, z, y, x;
+    t = getFloatFromUser("t: ");
+    z = getFloatFromUser("z: ");
+    y = calculateY(t, z);
+    x = calculateX(t, z, y);
+    cout << "t = " << t << endl;
+    cout << "z = " << z << endl;
+    cout << "y = " << y << endl;
+    cout << "x = " << x << endl;
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
