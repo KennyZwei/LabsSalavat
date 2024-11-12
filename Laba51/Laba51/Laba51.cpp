c041ae99982514c7094a961b6658f35b4707c9da
KennyAin <narbal199111111@gmail.com>
2024-11-12 19:30:52 +0300

    Laba 5 part 1

---
+++ Laba51/Laba51/Laba51.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+float getFloatFromUser(string title)
+{
+    char* end;
+    string input;
+    cout << title;
+    cin >> input;
+    float value = strtof(input.c_str(), &end);
+    while (*end)
+    {
+        cout << "Invalid input. Please enter an float: ";
+        cin >> input;
+        value = strtof(input.c_str(), &end);
+    }
+    return value;
+}
+
+int main()
+{
+    float number, S = 0;
+
+    do {
+        number = getFloatFromUser("number: ");
+        S += number;
+    } while (number != 0);
+
+    cout << "Sum = " << S << endl;
+    return 0;
+}
