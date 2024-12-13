9260bb998ae7c157809212ff9f5d108588fa10cf
KennyAin <narbal199111111@gmail.com>
2024-12-13 19:31:28 +0300

    Laba 7 part 1

---
+++ Laba71/Laba71/Laba71.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+int AddLeftDigit(int D, int K)
+{
+    int digitsNumber = 0;
+    int tempValue = K;
+    do {
+        digitsNumber++;
+        tempValue /= 10;
+    } while (tempValue != 0);
+
+    return K + D * pow(10, digitsNumber);
+}
+
+void init_arr(int la[], int DIM)
+{
+    for (int i = 0; i < DIM; i++)
+    {
+        la[i] = ((((long)i << 4) << 16) + i * 3);
+        cout << "la[" << i << "] = " << la[i] << endl;
+    }
+}
+
+int bits(int la[], int DIM)
+{
+    int j, bitCount, i, ;
+    for (i = 0; i < DIM; i++)
+    {
+        bitCount = 0;
+        for (j = 0; j < 8 * sizeof(int); j++) {
+            if ((la[i] & (1L << j)) != 0) {
+                bitCount++;
+            }
+        }
+        cout << "la[" << i << "] count bits setted to 1 = " << bitCount << endl;
+    }
+}
+
+int main()
+{
+    const int DIM = 25;
+    int a[DIM];
+    init_arr(a, DIM);
+    bits(a, DIM);
+    return 0;
+}
