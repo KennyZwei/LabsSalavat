f82dea570bc6c393e379b13179ab9cab69e50601
KennyAin <narbal199111111@gmail.com>
2024-12-18 20:08:48 +0300

    Laba 6 part 2

--- Laba62/Laba62/Laba62.cpp
+++ Laba62/Laba62/Laba62.cpp
@@ -1,39 +1,24 @@
-﻿#define _USE_MATH_DEFINES
-#include <iostream>
-#include <string>
-#include <cmath>
+﻿#include<iostream>
+#include<cmath>
 using namespace std;
-
-int rand_int(int M, int N)
-{
-    return rand() % (N - M + 1) + M;
-}
-
-int AddLeftDigit(int D, int K)
-{
-    int digitsNumber = 0;
-    int tempValue = K;
-    do {
-        digitsNumber++;
-        tempValue /= 10;
-    } while (tempValue != 0);
-
-    return K + D * pow(10, digitsNumber);
-}
-
+float prog(float a, int n);
 int main()
 {
-    srand(time(0));
-    int D;
-    int numberCount = 2;
-    int K = rand_int(0, 9);
-    cout << "K = " << K << endl << endl;
-    for (int i = 0; i < numberCount; i++) {
-        D = rand_int(0, 9);
-        cout << "D = " << D << endl;
-        
-        K = AddLeftDigit(D, K);
-        cout << "K = " << K << endl << endl;
-    }
-    return 0;
+	float tn = -3e0, tk = 0.3e1, dt = 2;
+	int n = 2;
+	cout << "\n\tt" << "\ty\n\n";
+	for (float t = tn;t < tk + dt / 2;t += dt)
+		cout << "\t" << t << "\t" << prog(t, n) << endl;
+	system("pause");
+	return 0;
+}
+float prog(float a, int nn)
+{
+	float fi = 2.e1, u = 1.;
+	for (int i = 1;i <= nn;i++)
+	{
+		u *= a * a;
+		fi -= u / i;
+	}
+	return fi;
 }
