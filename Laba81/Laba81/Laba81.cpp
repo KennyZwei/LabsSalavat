fa81e2e5b3a01d3d5cad186ddd11590f0034e9e2
KennyAin <narbal199111111@gmail.com>
2024-12-18 20:12:41 +0300

    Laba 8 part 1

--- Laba81/Laba81/Laba81.cpp
+++ Laba81/Laba81/Laba81.cpp
@@ -36,7 +36,6 @@ float getFloatFromUser(string title)
 
 int main()
 {
-    int negativeCount = 0, positiveCount = 0, BIndex = 0, CIndex = 0;
 
     int N = getIntFromUser("N: ");
     float* A = new float[N];
@@ -44,35 +43,16 @@ int main()
     
     for (int i = 0; i < N; i++) {
         A[i] = getFloatFromUser("A[" + to_string(i) + "]: ");
-        if (A[i] >= 0) {
-            positiveCount++;
-        }
-        else {
-            negativeCount++;
-        }
     }
     cout << endl;
 
-    float* B = new float[positiveCount];
-    float* C = new float[negativeCount];
     for (int i = 0; i < N; i++) {
-        if (A[i] >= 0) {
-            B[BIndex] = A[i];
-            BIndex++;
-        }
-        else {
-            C[CIndex] = A[i];
-            CIndex++;
-        }
-    }
-
-    for (int i = 0; i < negativeCount; i++) {
-        cout << "B[" << i << "] = " << B[i] << endl;
+        A[i] = A[i] + i;
     }
     cout << endl;
 
-    for (int i = 0; i < positiveCount; i++) {
-        cout << "C[" << i << "] = " << C[i] << endl;
+    for (int i = 0; i < N; i++) {
+        cout << "A[" << i << "] = " << A[i] << endl;
     }
     cout << endl;
     return 0;
