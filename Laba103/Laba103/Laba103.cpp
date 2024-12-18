83255b9a80ee06eec39ae82f9143e4da75f34ae2
KennyAin <narbal199111111@gmail.com>
2024-12-18 20:56:16 +0300

    Laba 10 part 3

---
+++ Laba103/Laba103/Laba103.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(void)
+{
+    int rowCount = 4;
+
+    for (int i = 0; i < rowCount; i++) {
+        cout << setw(rowCount - i);
+        for (int j = 0; j < i * 2 + 1; j++) {
+            cout << '*';
+        }
+        cout << endl;
+    }
+    return 0;
+}
